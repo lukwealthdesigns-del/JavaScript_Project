e0e10e4c5ecb507c0247c3fbdb5a4eb64c1b0b2f
Mariam <madesina011@gmail.com>
2025-09-29 16:04:22 +0100

    second

--- project_stafflogin/script.js
+++ project_stafflogin/script.js
@@ -1,15 +1,21 @@
 function loginPageRequirement() {
-    let userName= document.getElementById("username").value;
-    let password= document.getElementById("password").value;
+    let userName = document.getElementById("username").value.trim();
+    let password = document.getElementById("password").value.trim();
 
-    if (userName ==="" && password === "") {
-        console.log("You have to insert your details")
-
+    if (userName === "" && password === "") {
+        console.log("You have to insert your details");
+        alert("Please enter username and password!");
     } else if (userName === "admin" && password === "cafe123") {
-        console.log("welcome to freshbites!")
-        
-    } else {
-        alert("Invalid")
-    }
+        console.log("Welcome to FreshBite!");
 
+        // Save login info in localStorage
+        localStorage.setItem("isLoggedIn", "true");
+        localStorage.setItem("loggedUser", userName);
+
+        alert("Login successful!");
+        // Redirect to staff dashboard (you can create dashboard.html)
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Invalid credentials, try again!");
+    }
 }
