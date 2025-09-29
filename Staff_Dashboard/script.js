d52c64a1bcf0ce9506d5544a65c2a4cbfa5c0104
Lukman Ibrahim <lukwealthdesigns@gmail.com>
2025-09-29 16:25:07 +0100

    latest

---
+++ Staff_Dashboard/script.js
@@ -0,0 +1,7 @@
+// Show current date
+document.getElementById("date").innerText = new Date().toDateString();
+// Logout function
+function logout() {
+    alert("Logging out...");
+    // Redirect logic goes here
+}
