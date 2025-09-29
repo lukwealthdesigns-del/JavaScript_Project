1c8cb5a75f42f5cafec22eaff21c1d9022fec879
Oyedoyin Samuel <sammmie225@gmail.com>
2025-09-29 13:40:15 +0100

    m

---
+++ project/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementsByClassName("btn");
+const cart = document.getElementById("cart");
+const total = document.getElementById("total");
+function additems(button){
+
+}
